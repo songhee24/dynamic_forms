ae0c8eb503553de5c8f45f8e0352915cd4e586c2
songhee24 <akaratel61@gmail.com>
2023-03-15 14:24:22 +0600

    some basic logic done

--- lib/screens/dynamic_contact_forms.dart
+++ lib/screens/dynamic_contact_forms.dart
@@ -1,13 +1,16 @@
+import 'package:dynamic_forms/widgets/contact_form.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 ///  Created by mac on 15/3/23.
 class DynamicContactForms extends StatelessWidget {
+  List<ContactForm> contactForms = List.empty(growable: true);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Create Dynamic Contacts"),
+        title: const Text("Create Dynamic Contacts"),
       ),
       bottomNavigationBar: Padding(
         padding: const EdgeInsets.all(8.0),
@@ -16,16 +19,23 @@ class DynamicContactForms extends StatelessWidget {
           onPressed: () {
             // onSave();
           },
-          child: Text("Save"),
+          child: const Text("Save"),
         ),
       ),
       floatingActionButton: FloatingActionButton(
         backgroundColor: Colors.orange,
-        child: Icon(Icons.add),
+        child: const Icon(Icons.add),
         onPressed: () {
           // onAdd();
         },
       ),
+      body: contactForms.isNotEmpty
+          ? ListView.builder(
+              itemCount: contactForms.length,
+              itemBuilder: (_, index) {
+                return contactForms[index];
+              })
+          : const Center(child: Text("Tap on + to Add Contact")),
     );
   }
 }
